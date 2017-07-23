4d7c2d7a6311d17fc0b84f3dc1b817237c792068
Derrick Hammer <derrick@derrickhammer.com>
2017-07-23 19:23:04 -0400

    *Add WPRocketLazyload rewrite

--- lib/Rocket/Footer/JS/Rewrite/Manager.php
+++ lib/Rocket/Footer/JS/Rewrite/Manager.php
@@ -7,5 +7,6 @@ use Rocket\Footer\JS\ManagerAbstract;
 class Manager extends ManagerAbstract {
 	protected $modules = [
 		'Tawkto',
+		'WPRocketLazyload',
 	];
 }
---
+++ lib/Rocket/Footer/JS/Rewrite/WPRocketLazyload.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Rocket\Footer\JS\Rewrite;
+
+
+class WPRocketLazyload extends RewriteAbstract {
+
+	/**
+	 * @param string  $content
+	 *
+	 * @param  string $src
+	 *
+	 * @return void
+	 */
+	protected function do_rewrite( $content, $src ) {
+		if ( preg_match( '~\(function\(\s*w\s*,\s*d\s*\)\s*{\s*.*b\.src\s*=\s*"(.*)"\s*;.*\(\s*window,document\s*\)\s*;~s', $content, $matches ) ) {
+			$this->inject_tag( $this->create_script( null, $matches[1] ) );
+			$this->tags->remove();
+		}
+	}
+}
