69e526dff60d1d59af86195eab9c3c5c3be4d9e1
Derrick Hammer <derrick@derrickhammer.com>
2017-07-23 19:20:43 -0400

    *Use inject_tag and create_script methods

--- lib/Rocket/Footer/JS/Rewrite/Tawkto.php
+++ lib/Rocket/Footer/JS/Rewrite/Tawkto.php
@@ -15,11 +15,7 @@ class Tawkto extends RewriteAbstract {
 	 */
 	protected function do_rewrite( $content, $src ) {
 		if ( preg_match( '~var\s*Tawk_API\s*=\s*Tawk_API.*s1.src\s*=\s*\'(.*)\';.*s0\.parentNode\.insertBefore\(s1,s0\);\s*}\s*\)\(\);~sU', $content, $matches ) ) {
-			$external_tag = $this->document->createElement( 'script' );
-			$external_tag->setAttribute( 'type', 'text/javascript' );
-			$external_tag->setAttribute( 'src', "{$matches[1]}" );
-			$external_tag->setAttribute( 'async', false );
-			$this->tags->current()->parentNode->insertBefore( $external_tag, $this->tags->current() );
+			$this->inject_tag( $this->create_script( null, $matches[1] ) );
 			$this->tags->remove();
 		}
 	}
