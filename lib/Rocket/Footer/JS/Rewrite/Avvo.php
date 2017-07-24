5e0436c57293f427498497100bee6d4b87a363c6
Derrick Hammer <derrick@derrickhammer.com>
2017-07-24 08:06:34 -0400

    *Add Avvo rewrite

---
+++ lib/Rocket/Footer/JS/Rewrite/Avvo.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Rocket\Footer\JS\Rewrite;
+
+
+class Avvo extends RewriteAbstract {
+
+	/**
+	 * @param string $content
+	 *
+	 * @param string $src
+	 *
+	 * @return void
+	 */
+	protected function do_rewrite( $content, $src ) {
+		if ( preg_match( '~\(\s*function\s*\(\s*\)\s*{\s*setTimeout\s*\(\s*function\s*\(\s*\){\s*var\s*s\s*=\s*.*s\s*\.\s*src="((?:https?:)?//ia\s*.avvo\s*.com/tracker/\w+.js)".*}\s*\)\s*\(\s*\)\s*;~s', $content, $matches ) ) {
+			$this->inject_tag( $this->create_script( null, $matches[1] ) );
+			$this->tags->remove();
+		}
+	}
+}
