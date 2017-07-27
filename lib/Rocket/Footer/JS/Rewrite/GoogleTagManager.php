775045f7e437ece1f9f3c176f979bce47356cb5f
Derrick Hammer <derrick@derrickhammer.com>
2017-07-27 17:03:17 -0400

    *Fix GTM regex

--- lib/Rocket/Footer/JS/Rewrite/GoogleTagManager.php
+++ lib/Rocket/Footer/JS/Rewrite/GoogleTagManager.php
@@ -14,7 +14,7 @@ class GoogleTagManager extends RewriteAbstract {
 	 * @return void
 	 */
 	protected function do_rewrite( $content, $src ) {
-		if ( preg_match( '~\(\s*function\s*\(\s*w\s*,\s*\s*d,\s*s,\s*l,\s*i\s*\)\s*\{\s*w\s*\[\s*l\s*\]\s*=\s*w\s*\[\s*l\]\s*\|\|\s*\[\s*\]\s*;\s*w\s*\[\s*l\s*\].*\(\s*window\s*,\s*document\s*,\s*\'script\'\s*,\s*\'.*\'\s*,\s*\'(GTM-TPLZ9H)\'\s*\);~s', $content, $matches ) ) {
+		if ( preg_match( '~\(\s*function\s*\(\s*w\s*,\s*\s*d,\s*s,\s*l,\s*i\s*\)\s*\{\s*w\s*\[\s*l\s*\]\s*=\s*w\s*\[\s*l\]\s*\|\|\s*\[\s*\]\s*;\s*w\s*\[\s*l\s*\].*\(\s*window\s*,\s*document\s*,\s*\'script\'\s*,\s*\'.*\'\s*,\s*\'([\w]+-[\w]+)\'\s*\);~s', $content, $matches ) ) {
 			$this->inject_tag( $this->create_script( null, add_query_arg( 'id', $matches[1], 'https://www.googletagmanager.com/gtm.js' ) ) );
 			$this->tags->remove();
 		}
