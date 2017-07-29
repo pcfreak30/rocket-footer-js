aff9d25203ed0af93e05e5572545ef71446889a2
Derrick Hammer <derrick@derrickhammer.com>
2017-07-29 00:58:29 -0400

    *Use external CDN copy of base64.js

--- lib/Rocket/Footer/JS/Lazyload/BlogHerAds.php
+++ lib/Rocket/Footer/JS/Lazyload/BlogHerAds.php
@@ -39,7 +39,7 @@ class BlogHerAds extends LazyloadAbstract {
 				} while ( null !== $prev_tag && XML_ELEMENT_NODE !== $prev_tag->nodeType && 'script' !== strtolower( $tag->tagName ) );
 				$js_tag = $prev_tag;
 				if ( ! $this->base64_injected ) {
-					$this->inject_tag( $this->create_script( '!function(){function e(e){this.message=e}var t="undefined"!=typeof exports?exports:"undefined"!=typeof self?self:$.global,r="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";e.prototype=new Error,e.prototype.name="InvalidCharacterError",t.btoa||(t.btoa=function(t){for(var o,n,a=String(t),i=0,f=r,c="";a.charAt(0|i)||(f="=",i%1);c+=f.charAt(63&o>>8-i%1*8)){if(n=a.charCodeAt(i+=.75),n>255)throw new e("\'btoa\' failed: The string to be encoded contains characters outside of the Latin1 range.");o=o<<8|n}return c}),t.atob||(t.atob=function(t){var o=String(t).replace(/[=]+$/,"");if(o.length%4==1)throw new e("\'atob\' failed: The string to be decoded is not correctly encoded.");for(var n,a,i=0,f=0,c="";a=o.charAt(f++);~a&&(n=i%4?64*n+a:a,i++%4)?c+=String.fromCharCode(255&n>>(-2*i&6)):0)a=r.indexOf(a);return c})}();' ) );
+					$this->inject_tag( $this->create_script( null, 'https://cdnjs.cloudflare.com/ajax/libs/Base64/1.0.1/base64.min.js' ) );
 					$this->base64_injected = true;;
 				}
 
