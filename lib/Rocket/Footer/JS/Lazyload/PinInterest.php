398e69349c324515333c0f0b1308e507b956e6e8
Derrick Hammer <derrick@derrickhammer.com>
2017-08-01 17:35:53 -0400

    *Only lazy load widget markup and minify main script one time

--- lib/Rocket/Footer/JS/Lazyload/PinInterest.php
+++ lib/Rocket/Footer/JS/Lazyload/PinInterest.php
@@ -7,6 +7,7 @@ namespace Rocket\Footer\JS\Lazyload;
 use Rocket\Async\CSS\DOMElement;
 
 class PinInterest extends LazyloadAbstract {
+	private $injected = false;
 
 	/**
 	 * @param string  $content
@@ -17,17 +18,18 @@ class PinInterest extends LazyloadAbstract {
 	 */
 	protected function do_lazyload( $content, $src ) {
 		if ( 'assets.pinterest.com' === parse_url( $src, PHP_URL_HOST ) && false !== strpos( parse_url( $src, PHP_URL_PATH ), 'js/pinit.js' ) ) {
-
 			/** @var DOMElement $tag */
 			foreach ( $this->xpath->query( '//*[@data-pin-do]' ) as $tag ) {
-				$tag->setAttribute( 'data-lazy-widget', 'pin-interest' );
-				if ( 0 == $tag->childNodes->length ) {
-					$img = $this->create_pixel_image();
-					$tag->setAttribute( 'data-lazy-widget', 'pin-interest' );
-					$tag->appendChild( $img );
-				}
+				$img = $this->create_pixel_image();
+				$tag->setAttribute( 'data-lazy-widget', "pin-interest-{$this->instance}" );
+				$tag->parentNode->insertBefore( $tag, $img );
+				$this->lazyload_script( $this->get_script_content( $tag ), "pin-interest-{$this->instance}", $tag );
+				$this->instance ++;
 			}
-			$this->lazyload_script( $this->get_script_content( $this->create_script( null, 'https://assets.pinterest.com/js/pinit_main.js' ) ), 'pin-interest' );
+			if ( ! $this->injected ) {
+				$this->inject_tag( $this->create_script( null, 'https://assets.pinterest.com/js/pinit_main.js' ) );
+				$this->injected = true;
+			}
 			$this->tags->remove();
 		}
 	}
