50d0b8ec91c7f768bb7a3a993af612a297975a68
Derrick Hammer <derrick@derrickhammer.com>
2017-08-03 10:40:46 -0400

    *Add integration with McAfeeSecure plugin to render conversion script as plain html ready for minification

---
+++ lib/Rocket/Footer/JS/Integration/McAfeeSecure.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace Rocket\Footer\JS\Integration;
+
+
+class McAfeeSecure implements IntegrationInterface {
+
+	public function init() {
+		if ( class_exists( 'Mcafeesecure' ) && class_exists( 'WooCommerce' ) ) {
+			remove_action( 'woocommerce_thankyou', 'Mcafeesecure::inject_sip_modal' );
+			add_action( 'woocommerce_thankyou', [ $this, 'inject_sip_modal' ] );
+		}
+	}
+
+	public function inject_sip_modal( $order_id ) {
+		echo <<<EOT
+            <script type="text/javascript" src="https://www.mcafeesecure.com/js/conversion.js" async="async"></script>
+            <script type="text/javascript" class="mcafeesecure-track-conversion" data-type="purchase" data-orderid="$$order_id}" data-no-minify="1"></script>
+EOT;
+	}
+}
