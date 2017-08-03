4e5879eafaf9a21bb4309d4c69928ce6ccbdef69
Derrick Hammer <derrick@derrickhammer.com>
2017-08-02 21:26:00 -0400

    *Fix create_script call

--- lib/Rocket/Footer/JS/Rewrite/McAfeeSecure.php
+++ lib/Rocket/Footer/JS/Rewrite/McAfeeSecure.php
@@ -17,7 +17,7 @@ class McAfeeSecure extends RewriteAbstract {
 		if ( 'cdn.ywxi.net' === parse_url( $src, PHP_URL_HOST ) || false !== strpos( $content, '.ywxi.net/js/1.js' ) ) {
 			$file = rocket_footer_js()->remote_fetch( add_query_arg( 'h', parse_url( home_url(), PHP_URL_HOST ), 'https://cdn.ywxi.net/js/host-loader.js' ) );
 			if ( ! empty( $file ) && preg_match( '~host.js\?v=\d+&h=[\w\.-]+~', $file, $matches ) ) {
-				$external_script = $this->create_script( "https://cdn.ywxi.net/js/{$matches[0]}" );
+				$external_script = $this->create_script( null, "https://cdn.ywxi.net/js/{$matches[0]}" );
 				$this->inject_tag( $external_script );
 				$this->tags->remove();
 			}
