9c77f8e0e59bd30f74cbc57010fe3aec7b1b5f8f
Derrick Hammer <derrick@derrickhammer.com>
2017-08-12 04:57:41 -0400

    *Ensure _prum js variable exists

--- lib/Rocket/Footer/JS/Rewrite/PingdomPrum.php
+++ lib/Rocket/Footer/JS/Rewrite/PingdomPrum.php
@@ -15,6 +15,7 @@ class PingdomPrum extends RewriteAbstract {
 	 */
 	protected function do_rewrite( $content, $src ) {
 		if ( false !== strpos( $content, 'rum-static.pingdom.net/prum.min.js' ) ) {
+			$this->inject_tag( $this->create_script( "_prum = typeof _prum !== 'undefined' ? _prum : [];" ) );
 			$this->inject_tag( $this->create_script( null, 'https://rum-static.pingdom.net/prum.min.js' ) );
 			$this->tags->remove();
 		}
