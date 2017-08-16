05f3faff765eb98d1f2298c59b052b82e79e9b62
Derrick Hammer <derrick@derrickhammer.com>
2017-08-16 02:08:20 -0400

    *Rewrite tawkto integration to use new api's

--- lib/Rocket/Footer/JS/Integration/Tawkto.php
+++ lib/Rocket/Footer/JS/Integration/Tawkto.php
@@ -4,8 +4,6 @@
 namespace Rocket\Footer\JS\Integration;
 
 
-use Rocket\Footer\JS;
-
 class Tawkto extends IntegrationAbstract {
 
 	public function init() {
@@ -15,32 +13,34 @@ class Tawkto extends IntegrationAbstract {
 	}
 
 	public function process( $script, $url ) {
-		global $rocket_async_css_file;
 		if ( 'embed.tawk.to' === parse_url( $url, PHP_URL_HOST ) ) {
-			$this->content_document = rocket_footer_js()->get_script_document();
-			$this->tags             = rocket_footer_js()->get_dom_collection();
+			$this->content_document = $this->plugin->script_document;
+			$this->tags             = $this->plugin->dom_collection;
 			if ( preg_match( '~\w\.src\s*=\s*"(https://cdn\.jsdelivr\.net/emojione/[\d\.]+/lib/js/emojione\.min\.js)"\s*;~', $script, $matches ) ) {
 				$script = str_replace( $matches[0], '', $script );
 				$this->tags->add( $this->create_script( null, $matches[1] ) );
 			}
-			if ( ! empty( $rocket_async_css_file ) && class_exists( 'Rocket_Async_Css' ) && method_exists( 'Rocket_Async_Css', 'minify_remote_file' ) && preg_match( '~\w\.href\s*=\s*"(https://cdn\.jsdelivr\.net/emojione/[\d\.]+/assets/css/emojione\.min\.css)"\s*;~', $script, $matches ) ) {
-				$script        = str_replace( $matches[0], '', $script );
-				$style         = rocket_footer_js()->get_content( $rocket_async_css_file );
-				$item_cache_id = md5( $matches[1] );
-				$store         = rocket_footer_js()->get_cache_manager()->get_store();
-				$store->set_prefix( Rocket_Async_Css::TRANSIENT_PREFIX );
-				$file = $store->get_cache_fragment( $item_cache_id );
-				if ( empty( $file ) ) {
-					$file = rocket_footer_js()->remote_fetch( $matches[1] );
+			if ( function_exists( 'rocket_async_css_instance' ) && preg_match( '~\w\.href\s*=\s*"(https://cdn\.jsdelivr\.net/emojione/[\d\.]+/assets/css/emojione\.min\.css)"\s*;~', $script, $matches ) ) {
+				$script = str_replace( $matches[0], '', $script );
+				$files  = rocket_async_css_instance()->get_files();
+				if ( ! empty( $files['all'] ) ) {
+					$style         = $this->plugin->get_content( $files['all'] );
+					$item_cache_id = md5( $matches[1] );
+					$store         = $this->plugin->cache_manager->get_store();
+					$store->set_prefix( rocket_async_css_instance()->get_transient_prefix() );
+					$file = $store->get_cache_fragment( $item_cache_id );
+					if ( empty( $file ) ) {
+						$file = $this->plugin->remote_fetch( $matches[1] );
+					}
+					// Do nothing on error
+					if ( ! empty( $file ) ) {
+						$css_part = rocket_async_css_instance()->minify_remote_file( $url, $file );
+						$style    .= $css_part;
+						$store->update_cache_fragment( $item_cache_id, $css_part );
+						$this->plugin->put_content( $files['all'], $style );
+					}
+					$store->set_prefix( $this->plugin->get_transient_prefix() );
 				}
-				// Do nothing on error
-				if ( ! empty( $file ) ) {
-					$css_part = Rocket_Async_Css::get_instance()->minify_remote_file( $url, $file );
-					$style    .= $css_part;
-					$store->update_cache_fragment( $item_cache_id, $css_part );
-					rocket_footer_js()->put_content( $rocket_async_css_file, $style );
-				}
-				$store->set_prefix( JS::TRANSIENT_PREFIX );
 			}
 		}
 
