74268b5dd9bdf0e49bcd4236f6a4231ac8bb759e
Derrick Hammer <derrick@derrickhammer.com>
2017-08-22 17:51:21 -0400

    *Skip re-processing of crazyegg

--- lib/Rocket/Footer/JS/Integration/CrazyEgg.php
+++ lib/Rocket/Footer/JS/Integration/CrazyEgg.php
@@ -11,6 +11,7 @@ class CrazyEgg extends IntegrationAbstract {
 	 */
 	public function init() {
 		add_filter( 'rocket_footer_js_process_remote_script', [ $this, 'process' ], 10, 2 );
+		add_filter( 'rocket_footer_js_reprocess_local_script', [ $this, 'maybe_reprocess' ] );
 	}
 
 	public function process( $script, $url ) {
@@ -21,4 +22,13 @@ class CrazyEgg extends IntegrationAbstract {
 
 		return $script;
 	}
+
+	public function maybe_reprocess( $value, $script, $url ) {
+		if ( 'script.crazyegg.com' === parse_url( $url, PHP_URL_HOST ) && strpos( $script, 'atob' ) ) {
+			$value = false;
+		}
+
+		return $value;
+	}
+
 }
