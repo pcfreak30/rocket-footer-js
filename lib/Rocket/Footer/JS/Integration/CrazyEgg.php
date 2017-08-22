0ea74b0ee087d73913c18f55fcb8d1dc450af17e
Derrick Hammer <derrick@derrickhammer.com>
2017-08-22 18:20:57 -0400

    *Wrong reprocess filter used

--- lib/Rocket/Footer/JS/Integration/CrazyEgg.php
+++ lib/Rocket/Footer/JS/Integration/CrazyEgg.php
@@ -11,7 +11,7 @@ class CrazyEgg extends IntegrationAbstract {
 	 */
 	public function init() {
 		add_filter( 'rocket_footer_js_process_remote_script', [ $this, 'process' ], 10, 2 );
-		add_filter( 'rocket_footer_js_reprocess_local_script', [ $this, 'maybe_reprocess' ] );
+		add_filter( 'rocket_footer_js_reprocess_remote_script', [ $this, 'maybe_reprocess' ] );
 	}
 
 	public function process( $script, $url ) {
