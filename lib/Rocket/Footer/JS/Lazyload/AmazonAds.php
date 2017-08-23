4031692e5a4990aef71695e8a603c97ed608ee19
Derrick Hammer <derrick@derrickhammer.com>
2017-08-23 01:09:53 -0400

    *Use prev method

--- lib/Rocket/Footer/JS/Lazyload/AmazonAds.php
+++ lib/Rocket/Footer/JS/Lazyload/AmazonAds.php
@@ -14,43 +14,36 @@ class AmazonAds extends LazyloadAbstract {
 	 * @return void
 	 */
 	protected function do_lazyload( $content, $src ) {
-		$tag      = $this->tags->current();
-		$prev_tag = $tag;
-		do {
-			$prev_tag = $prev_tag->previousSibling;
-		} while ( null !== $prev_tag && XML_ELEMENT_NODE !== $prev_tag->nodeType && 'script' !== strtolower( $tag->tagName ) );
-		$sub_content = $this->get_script_content() . $this->get_tag_content( $prev_tag );
-		$img         = $this->create_pixel_image();
-		$span        = $this->create_tag( 'span' );
-		$span->setAttribute( 'data-lazy-widget', "amazon-ads-{$this->instance}" );
-		$span->appendChild( $img );
-		$prev_tag->parentNode->removeChild( $prev_tag );
-		$tag->parentNode->insertBefore( $span, $tag );
-		$this->lazyload_script( $sub_content, "amazon-ads-{$this->instance}" );
-		$tag->setAttribute( 'data-lazy-widget', "amazon-ads-{$this->instance}" );
-		$this->instance ++;
-		$this->tags->flag_removed();
+		$tag     = $this->tags->current();
+		$js_node = $tag->prev( "script" );
+		if ( ! empty( $js_node ) ) {
+			$sub_content = $this->get_script_content() . $this->get_tag_content( $js_node );
+			$img         = $this->create_pixel_image();
+			$span        = $this->create_tag( 'span' );
+			$span->setAttribute( 'data-lazy-widget', "amazon-ads-{$this->instance}" );
+			$span->appendChild( $img );
+			$js_node->parentNode->removeChild( $js_node );
+			$tag->parentNode->insertBefore( $span, $tag );
+			$this->lazyload_script( $sub_content, "amazon-ads-{$this->instance}" );
+			$tag->setAttribute( 'data-lazy-widget', "amazon-ads-{$this->instance}" );
+			$this->instance ++;
+			$this->tags->flag_removed();
+		}
+
 	}
 
 	protected function do_lazyload_off( $content, $src ) {
 
 		$this->set_no_minify();
-		$tag      = $this->tags->current();
-		$prev_tag = $tag;
-		do {
-			$prev_tag = $prev_tag->previousSibling;
-		} while ( null !== $prev_tag && XML_ELEMENT_NODE !== $prev_tag->nodeType && 'script' !== strtolower( $tag->tagName ) );
-		$js_node = $prev_tag;
+		$tag     = $this->tags->current();
+		$js_node = $tag->prev( "script" );
 		if ( ! empty( $js_node ) ) {
-			$this->set_no_minify( $prev_tag );
+			$this->set_no_minify( $js_node );
 		}
 
 	}
 
-	protected
-	function is_match(
-		$content, $src
-	) {
+	protected function is_match( $content, $src ) {
 		return parent::is_match( $content, $src ) && false !== strpos( $src, 'amazon-adsystem.com' );
 	}
 }
