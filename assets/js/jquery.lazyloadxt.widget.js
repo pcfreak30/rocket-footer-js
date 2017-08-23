3c8710eeda98ccfc8661412033ec09c0d8ca87ed
Derrick Hammer <derrick@derrickhammer.com>
2017-08-23 00:19:56 -0400

    *Add data attribute to remove lazy element on activate

--- assets/js/jquery.lazyloadxt.widget.js
+++ assets/js/jquery.lazyloadxt.widget.js
@@ -4,30 +4,36 @@
  * Licensed under MIT */
 
 (function ($) {
-  var options = $.lazyLoadXT,
-    widgetAttr = options.widgetAttr || 'data-lazy-widget',
-    reComment = /<!--([\s\S]*)-->/;
+	var options = $.lazyLoadXT,
+		widgetAttr = options.widgetAttr || 'data-lazy-widget',
+		reComment = /<!--([\s\S]*)-->/;
 
-  options.selector += ',[' + widgetAttr + ']';
+	options.selector += ',[' + widgetAttr + ']';
 
-  $(document).on('lazyshow', '[' + widgetAttr + ']', function () {
-    var $this = $(this),
-      $target = $this,
-      id = $this.attr(widgetAttr),
-      match;
+	$(document).on('lazyshow', '[' + widgetAttr + ']', function () {
+		var $this = $(this),
+			$target = $this,
+			id = $this.attr(widgetAttr),
+			match;
 
-    if (id) {
-      $target = $('#' + id);
-    }
-
-    if ($target.length) {
-      match = reComment.exec($target.html());
-      if (match) {
-        $target.replaceWith($.trim(match[ 1 ]));
-      }
-    }
-
-    $this.triggerHandler('load');
-  });
+		if (id) {
+			$target = $('#' + id);
+		}
+		var triggered = false;
+		if ($target.length) {
+			match = reComment.exec($target.html());
+			if (match) {
+				$target.replaceWith($.trim(match[ 1 ]));
+			}
+			if (1 === $this.data('lazy-remove')) {
+				$this.triggerHandler('load');
+				$this.remove();
+				triggered = true;
+			}
+		}
+		if (!triggered) {
+			$this.triggerHandler('load');
+		}
+	});
 
 })(window.jQuery || window.Zepto || window.$);
