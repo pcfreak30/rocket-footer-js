c9fc522fda58dd31c778334e390cd8105b70ccf1
Derrick Hammer <derrick@derrickhammer.com>
2017-11-30 19:10:24 -0500

    *Add dummy lazy load iframe js plugin to add iframes with data-src attributes

---
+++ assets/js/jquery.lazyloadxt.iframe.js
@@ -0,0 +1,10 @@
+/*jslint browser:true */
+/*jshint browser:true, jquery:true */
+
+(function ($) {
+	'use strict';
+
+	var options = $.lazyLoadXT;
+	options.selector += 'iframe[data-src]';
+
+})(window.jQuery || window.Zepto || window.$);
