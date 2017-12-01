9da6efd71437cbedeac78170614c76cb025c1f14
Derrick Hammer <derrick@derrickhammer.com>
2017-11-30 19:09:30 -0500

    *Add lazyload hack for divi video overlays

--- assets/js/jquery.lazyloadxt.videoembed.js
+++ assets/js/jquery.lazyloadxt.videoembed.js
@@ -34,5 +34,36 @@
 
 		$this.triggerHandler('load');
 	});
+	/* Divi Builder Video Overlay Workaround */
+	$(function () {
+		if ($('.et_pb_video_overlay').length) {
+			var old_et_pb_play_overlayed_video = null;
+			(function check () {
+				var func = window.et_pb_play_overlayed_video;
+				var is_overridden = func !== old_et_pb_play_overlayed_video;
+				var original_exists = old_et_pb_play_overlayed_video !== null;
+				if (func && (!is_overridden !== !original_exists)) {
+					old_et_pb_play_overlayed_video = window.et_pb_play_overlayed_video;
+					window.et_pb_play_overlayed_video = function ($play_video) {
+						var $this = $play_video,
+							$wrapper = $this.closest('.et_pb_video, .et_main_video_container, .et_pb_video_wrap'),
+							$image = $wrapper.find('.et_pb_video_box img');
+						$image.click();
+						var $video_iframe = $wrapper.find("iframe");
+						if ($video_iframe.length) {
+							$video_iframe.on('lazyload', function () {
+								if ($.fn.fitVids)
+									$wrapper.fitVids();
+								old_et_pb_play_overlayed_video($play_video)
+							});
+							return;
+						}
+						old_et_pb_play_overlayed_video($play_video);
+					}
+				}
+				setTimeout(check, 10);
+			})();
+		}
+	});
 
 })(window.jQuery || window.Zepto || window.$);
