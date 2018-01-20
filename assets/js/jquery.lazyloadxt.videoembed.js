54852857752f2eb318c687a74321021f45a6aa14
Derrick Hammer <derrick@derrickhammer.com>
2018-01-20 12:01:20 -0500

    *Dynamically render a video play overlay on video *Add compatibility with Visual Composer *Resize video container to the size of the commented iframe

--- assets/js/jquery.lazyloadxt.videoembed.js
+++ assets/js/jquery.lazyloadxt.videoembed.js
@@ -9,7 +9,35 @@
 		reComment = /<!--([\s\S]*)-->/;
 
 	options.selector += ',[' + widgetAttr + ']';
+	$(document).on('lazyload', '[' + widgetAttr + ']', function () {
+		var id = $(this).attr(widgetAttr),
+			match, $target = $();
 
+		if (id) {
+			$target = $('#' + id);
+		}
+		match = reComment.exec($target.html());
+		if (match) {
+			var html = $('<div>' + $.trim(match[ 1 ]) + '</div>');
+			if (html.find('iframe')) {
+				$(this).wrap($('<div />', {
+					'data-lazy-video-embed-container': $(this).data('lazyVideoEmbedType'),
+					width: html.find('iframe').attr('width'),
+					height: html.find('iframe').attr('height')
+				})).after($('<div />', { class: 'play' }));
+				var image = $(this);
+				$(this).siblings('.play').click(function () {
+					image.click();
+				})
+			}
+			// Force VC Composer Video element to have no padding and back up padding data
+			var vc_video_wrapper = $(this).closest('.wpb_video_wrapper');
+			if (vc_video_wrapper.length) {
+				vc_video_wrapper.data('videoPadding', vc_video_wrapper.css('padding-top'));
+				vc_video_wrapper.css('padding-top', '0px');
+			}
+		}
+	});
 	$(document).on('click', '[' + widgetAttr + ']', function () {
 		var $this = $(this),
 			$target = $this,
@@ -27,6 +55,18 @@
 				$target.remove();
 				if ($this !== $target) {
 					$this.remove();
+				}
+				//Restore VC Composer Video wrapper element padding
+				var vc_video_wrapper = $video.closest('.wpb_video_wrapper');
+				if (vc_video_wrapper.length) {
+					vc_video_wrapper.css('padding-top', vc_video_wrapper.data('videoPadding'));
+					vc_video_wrapper.removeData('videoPadding');
+				}
+				var embedContainer = $video.siblings('[data-lazy-video-embed-container]');
+				if (embedContainer.length) {
+					embedContainer.find('.play').remove();
+					embedContainer.parent().append(embedContainer.children());
+					embedContainer.remove();
 				}
 				$video.lazyLoadXT();
 			}
