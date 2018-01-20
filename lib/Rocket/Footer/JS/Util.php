256271d0f762a1cd59f4d1def0948fd6e29e2fee
Derrick Hammer <derrick@derrickhammer.com>
2018-01-20 11:49:49 -0500

    *refactor download_remote_file to return the 1st valid file from an array

--- lib/Rocket/Footer/JS/Util.php
+++ lib/Rocket/Footer/JS/Util.php
@@ -21,35 +21,39 @@ class Util extends ComponentAbstract {
 	}
 
 	/**
-	 * @param        $url
-	 * @param string $extension
+	 * @param array|string $urls
+	 * @param string       $extension
 	 *
 	 * @return string
 	 */
-	public function download_remote_file( $url, $extension = null ) {
-		$data = $this->plugin->remote_fetch( $url );
-		if ( ! empty( $data ) ) {
-			$url_parts = parse_url( $url );
-			$info      = pathinfo( $url_parts['path'] );
-			if ( empty( $url_parts['port'] ) ) {
-				$url_parts['port'] = '';
+	public function download_remote_file( $urls, $extension = null ) {
+		foreach ( (array) $urls as $url ) {
+			$data = $this->plugin->remote_fetch( $url );
+			if ( ! empty( $data ) ) {
+				$url_parts = parse_url( $url );
+				$info      = pathinfo( $url_parts['path'] );
+				if ( empty( $url_parts['port'] ) ) {
+					$url_parts['port'] = '';
+				}
+				if ( empty( $info['extension'] ) ) {
+					$info['extension'] = $extension;
+				}
+				if ( empty( $info['extension'] ) ) {
+					return $url;
+				}
+				$hash      = md5( $url_parts['scheme'] . '://' . $info['dirname'] . ( ! empty( $url_parts['port'] ) ? ":{$url_parts['port']}" : '' ) . '/' . $info['filename'] );
+				$filename  = $this->plugin->get_cache_path() . $hash . '.' . $info['extension'];
+				$final_url = get_rocket_cdn_url( set_url_scheme( str_replace( WP_CONTENT_DIR, WP_CONTENT_URL, $filename ) ) );
+				if ( ! $this->plugin->get_wp_filesystem()->is_file( $filename ) ) {
+					$this->plugin->put_content( $filename, $data );
+				}
+
+				return $final_url;
 			}
-			if ( empty( $info['extension'] ) ) {
-				$info['extension'] = $extension;
-			}
-			if ( empty( $info['extension'] ) ) {
-				return $url;
-			}
-			$hash      = md5( $url_parts['scheme'] . '://' . $info['dirname'] . ( ! empty( $url_parts['port'] ) ? ":{$url_parts['port']}" : '' ) . '/' . $info['filename'] );
-			$filename  = $this->plugin->get_cache_path() . $hash . '.' . $info['extension'];
-			$final_url = get_rocket_cdn_url( set_url_scheme( str_replace( WP_CONTENT_DIR, WP_CONTENT_URL, $filename ) ) );
-			if ( ! $this->plugin->get_wp_filesystem()->is_file( $filename ) ) {
-				$this->plugin->put_content( $filename, $data );
-			}
-			$url = $final_url;
+
 		}
 
-		return $url;
+		return false;
 	}
 
 	public function maybe_decode_script( $data ) {
