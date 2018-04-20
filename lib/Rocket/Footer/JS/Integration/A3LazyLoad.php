7132ae766870a141da8f049cb3a0214fc66d6ff7
Derrick Hammer <derrick@derrickhammer.com>
2018-04-20 04:49:13 -0400

    *Disable a3 lazy load if enabled but we are logged in and not caching logged in users, but allow filter rocket_footer_js_lazy_load_members_override to ovveride

--- lib/Rocket/Footer/JS/Integration/A3LazyLoad.php
+++ lib/Rocket/Footer/JS/Integration/A3LazyLoad.php
@@ -12,8 +12,13 @@ class A3LazyLoad extends IntegrationAbstract {
 	 *
 	 */
 	public function init() {
-		if ( $this->plugin->lazyload_manager->is_enabled() && 0 < (int) get_rocket_option( 'cdn' ) ) {
-			add_filter( 'a3_lazy_load_images_before', 'rocket_cdn_images' );
+		if ( $this->plugin->lazyload_manager->is_enabled() ) {
+			if ( 0 < (int) get_rocket_option( 'cdn' ) ) {
+				add_filter( 'a3_lazy_load_images_before', 'rocket_cdn_images' );
+			}
+			if ( is_user_logged_in() && ! ( 0 < get_rocket_option( 'cache_logged_user' ) ) && ! apply_filters( 'rocket_footer_js_lazy_load_members_override', false ) ) {
+				add_filter( 'a3_lazy_load_run_filter', '__return_false' );
+			}
 		}
 	}
 }
