05bb1d435c70463fe6521a1832e823f17372d596
Derrick Hammer <derrick@derrickhammer.com>
2018-08-11 13:08:46 -0400

    *Prevent any mediaelement embeds from auto starting for to lazy load by changing class names

--- lib/Rocket/Footer/JS/Integration/A3LazyLoad.php
+++ lib/Rocket/Footer/JS/Integration/A3LazyLoad.php
@@ -19,6 +19,23 @@ class A3LazyLoad extends IntegrationAbstract {
 			if ( is_user_logged_in() && ! ( 0 < get_rocket_option( 'cache_logged_user' ) ) && ! apply_filters( 'rocket_footer_js_lazy_load_members_override', false ) ) {
 				add_filter( 'a3_lazy_load_run_filter', '__return_false' );
 			}
+			add_filter( 'a3_lazy_load_videos_before', [ $this, 'lazyload_mediaelement_videos' ] );
 		}
+	}
+
+	public function lazyload_mediaelement_videos( $html ) {
+		$html = str_replace( [
+			'wp-audio-shortcode',
+			'wp-video-shortcode',
+			'wp-video-shortcode-lazyload-lazyload',
+			'wp-audio-shortcode-lazyload-lazyload',
+		], [
+			'wp-audio-shortcode-lazyload',
+			'wp-video-shortcode-lazyload',
+			'wp-audio-shortcode-lazyload',
+			'wp-video-shortcode-lazyload',
+		], $html );
+
+		return $html;
 	}
 }
