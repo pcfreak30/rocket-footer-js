281337c669acd4b0732140a8b0ae354aa829d65e
Derrick Hammer <derrick@derrickhammer.com>
2019-05-04 09:54:34 -0400

    *Add lazy load support for google maps widget *Merge google maps widget pro module to standard

--- lib/Rocket/Footer/JS/Integration/GoogleMapsWidget.php
+++ lib/Rocket/Footer/JS/Integration/GoogleMapsWidget.php
@@ -4,7 +4,9 @@
 namespace Rocket\Footer\JS\Integration;
 
 
-class GoogleMapsWidgetPRO extends IntegrationAbstract {
+class GoogleMapsWidget extends IntegrationAbstract {
+
+	private $options;
 
 	/**
 	 *
@@ -12,8 +14,11 @@ class GoogleMapsWidgetPRO extends IntegrationAbstract {
 	public function init() {
 		if ( class_exists( '\GMWP' ) ) {
 			add_action( 'wp_footer', [ $this, 'scripts' ], 11 );
+			if ( $this->plugin->lazyload_manager->is_enabled() ) {
+				$this->options = \GMWP::get_options();
+				add_filter( 'do_shortcode_tag', [ $this, 'process' ], 10, 2 );
+			}
 		}
-
 	}
 
 	public function scripts() {
@@ -25,5 +30,13 @@ class GoogleMapsWidgetPRO extends IntegrationAbstract {
 				wp_enqueue_style( 'gmw-colorbox', wp_unslash( $matches[2] ), [], null );
 			}
 		}
+	}
+
+	public function process( $output, $tag ) {
+		if ( $this->options['sc_map'] === $tag ) {
+			$output = apply_filters( 'a3_lazy_load_images', $output );
+		}
+
+		return $output;
 	}
 }
