4da8846e966f7ee2b07279350d5416ac51fd0f0b
Derrick Hammer <derrick@derrickhammer.com>
2019-05-24 22:14:30 -0400

    *remove compatibility script for master slider as it is no longer needed

--- lib/Rocket/Footer/JS/Integration/MasterSlider.php
+++ lib/Rocket/Footer/JS/Integration/MasterSlider.php
@@ -13,11 +13,6 @@ class MasterSlider extends IntegrationAbstract {
 			$settings                               = $this->a3_lazy_load_global_settings;
 			$settings['a3l_image_include_noscript'] = false;
 			$this->a3_lazy_load_global_settings     = $settings;
-			add_action( 'wp_enqueue_scripts', [ $this, 'scripts' ], 16 );
 		}
-	}
-
-	public function scripts() {
-		wp_add_inline_script( 'masterslider-core', '(function($){$(window).load(function(){$.each(window.masterslider_instances,function(index,instance){var cb=function(){$(window).lazyLoadXT();if(0===instance.slideController.currentSlide.$bg_img.data("lazied"))instance.slideController.addEventListener(MSSliderEvent.CHANGE_START,cb,instance);instance.slideController.addEventListener(MSSliderEvent.CHANGE_END,cb,instance)})})})(jQuery);' );
 	}
 }
