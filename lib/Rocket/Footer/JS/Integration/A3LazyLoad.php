bca71dd1bf9c76f68d84ab5ee90338b89fc78c76
Derrick Hammer <derrick@derrickhammer.com>
2019-05-29 20:20:14 -0400

    *Add further workarounds for a3 lazy load and wp-rocket to prevent bugs or clean up markup

--- lib/Rocket/Footer/JS/Integration/A3LazyLoad.php
+++ lib/Rocket/Footer/JS/Integration/A3LazyLoad.php
@@ -14,13 +14,15 @@ class A3LazyLoad extends IntegrationAbstract {
 			if ( 0 < (int) get_rocket_option( 'cdn' ) ) {
 				add_filter( 'a3_lazy_load_images_before', 'rocket_cdn_images' );
 			}
-			if ( is_user_logged_in() && ! ( 0 < get_rocket_option( 'cache_logged_user' ) ) && ! apply_filters( 'rocket_footer_js_lazy_load_members_override', false ) ) {
+			if ( ! is_admin() && is_user_logged_in() && ! ( 0 < get_rocket_option( 'cache_logged_user' ) ) && ! apply_filters( 'rocket_footer_js_lazy_load_members_override', false ) ) {
 				add_filter( 'a3_lazy_load_run_filter', '__return_false' );
 			}
 			add_filter( 'a3_lazy_load_videos_before', [ $this, 'lazyload_mediaelement_videos' ] );
 			add_filter( 'a3_lazy_load_videos_after', [ $this, 'remove_hidden_class' ] );
 			add_filter( 'a3_lazy_load_images_after', [ $this, 'remove_hidden_class' ] );
 			add_filter( 'a3_lazy_load_images_after', [ $this, 'remove_dummy_src' ] );
+			add_filter( 'a3_lazy_load_images_after', [ $this, 'remove_duplicate_srcset' ] );
+			add_filter( 'rocket_cdn_images_html', [ $this, 'fix_fake_src' ] );
 		}
 	}
 
@@ -41,10 +43,32 @@ class A3LazyLoad extends IntegrationAbstract {
 	}
 
 	public function remove_hidden_class( $html ) {
-		return str_replace( 'lazy lazy-hidden', 'lazyload', $html );
+		$html = str_replace( 'lazy lazy-hidden', 'lazyload', $html );
+		$html = str_replace( 'lazyload lazyload', 'lazyload', $html );
+		$html = str_replace( 'data-lazy-type="image"', '', $html );
+
+		return $html;
 	}
 
 	public function remove_dummy_src( $html ) {
-		return str_replace( 'src="' . A3_LAZY_LOAD_IMAGES_URL . '/lazy_placeholder.gif"', '', $html );
+		$placeholder     = A3_LAZY_LOAD_IMAGES_URL . '/lazy_placeholder.gif';
+		$placeholder_cdn = get_rocket_cdn_url( A3_LAZY_LOAD_IMAGES_URL . '/lazy_placeholder.gif', [ 'images' ], $placeholder );
+
+		$src_string      = ' src="%s"';
+		$fake_src_string = ' data-fake-src="%s"';
+
+		$html = str_replace( sprintf( $src_string, $placeholder ), sprintf( $fake_src_string, $placeholder ), $html );
+		$html = str_replace( sprintf( $src_string, $placeholder_cdn ), sprintf( $fake_src_string, $placeholder_cdn ), $html );
+
+		return $html;
 	}
+
+	public function fix_fake_src( $html ) {
+		return str_replace( 'data-fake- src', 'data-fake-src', $html );
+	}
+
+	public function remove_duplicate_srcset( $html ) {
+		return str_replace( 'data-srcset="" data-srcset=', 'data-srcset=', $html );
+	}
+
 }
