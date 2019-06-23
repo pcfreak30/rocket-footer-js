d1292171170c11a24d292c699ec545c73f88eb4c
Derrick Hammer <derrick@derrickhammer.com>
2019-06-23 01:43:31 -0400

    *Change container code for compatibility *Wrap majority of dom changes in a rAF function call

--- assets/js/jquery.lazyloadxt.videoembed.js
+++ assets/js/jquery.lazyloadxt.videoembed.js
@@ -10,68 +10,92 @@
 
 	options.selector += ',[' + widgetAttr + ']';
 	$(document).on('lazyload', '[' + widgetAttr + ']', function () {
-		var id = $(this).attr(widgetAttr),
+
+		var el = $(this), id = $(el).attr(widgetAttr),
 			match, $target = $();
 
 		if (id) {
 			$target = $('#' + id);
 		}
-		if ($(this).data('lazyLoadedVideo')) {
+		if ($(el).data('lazyLoadedVideo')) {
 			return false;
 		}
 		match = reComment.exec($target.html());
 		if (match) {
 			var html = $('<div>' + $.trim(match[ 1 ]) + '</div>');
 			if (html.find('iframe')) {
-				$(this).wrap($('<div />', {
-					'data-lazy-video-embed-container': $(this).data('lazyVideoEmbedType')
-				})).after($('<div />', { class: 'play' }));
-				var image = $(this);
-				image.parent().attr({
-					width: html.find('iframe').attr('width'),
-					height: html.find('iframe').attr('height')
-				});
-				$(this).siblings('.play').click(function () {
+				var parent = el.parent();
+				$(el).wrap($('<div />', {
+					'data-lazy-video-embed-container': $(el).data('lazyVideoEmbedType')
+				}))
+				$(el).after($('<div />', { class: 'play' }));
+				var image = $(el);
+				var width = html.find('iframe').attr('width');
+				if (width) {
+					image.parent().attr('width', width);
+				}
+				var height = html.find('iframe').attr('height');
+				if (height) {
+					image.parent().attr('height', height);
+				}
+
+				$(el).siblings('.play').click(function () {
 					image.click();
 				})
 			}
 			// Force VC Composer Video element to have no padding and back up padding data
-			var vc_video_wrapper = $(this).closest('.wpb_video_wrapper');
+			var vc_video_wrapper = $(el).closest('.wpb_video_wrapper');
 			if (vc_video_wrapper.length) {
-				vc_video_wrapper.data('videoPadding', vc_video_wrapper.css('padding-top'));
-				vc_video_wrapper.css('padding-top', '0px');
+				lazySizes.rAF(function () {
+					vc_video_wrapper.data('videoPadding', vc_video_wrapper.css('padding-top'));
+					vc_video_wrapper.css('padding-top', '0px');
+				});
 			}
 
-			var divi_video_wrapper = $(this).closest('.et_pb_video, .et_main_video_container, .et_pb_video_wrap');
+			var divi_video_wrapper = $(el).closest('.et_pb_video, .et_main_video_container, .et_pb_video_wrap');
 			if (divi_video_wrapper.length) {
-				$(this).parent().hide();
+				lazySizes.rAF(function () {
+					$(el).parent().hide();
+				});
 			}
 
-			var fusion_video_wrapper = $(this).closest('.fusion-video, .wpb_wrapper');
+			var fusion_video_wrapper = $(el).closest('.fusion-video, .wpb_wrapper');
 			if (fusion_video_wrapper.length && $.fn.fitVids) {
-				fusion_video_wrapper.fitVids({ customSelector: '[data-lazy-video-embed-container]' });
-				fusion_video_wrapper.find('.fluid-width-video-wrapper').removeAttr('style');
-				var fusion_privacy_placeholder = fusion_video_wrapper.find('.fusion-privacy-placeholder');
-				// Avada/Fusion privacy box compatibility
-				if (fusion_privacy_placeholder.length) {
-					fusion_privacy_placeholder.siblings('.fluid-width-video-wrapper').hide();
-					var padding,
-						parent = fusion_privacy_placeholder.parent(),
-						prev = fusion_privacy_placeholder.prev(),
-						width = fusion_privacy_placeholder.outerWidth(),
-						height = fusion_privacy_placeholder.outerHeight();
+				lazySizes.rAF(function () {
+					fusion_video_wrapper.fitVids({ customSelector: '[data-lazy-video-embed-container]' });
+					fusion_video_wrapper.find('.fluid-width-video-wrapper').removeAttr('style');
+					var fusion_privacy_placeholder = fusion_video_wrapper.find('.fusion-privacy-placeholder');
+					// Avada/Fusion privacy box compatibility
+					if (fusion_privacy_placeholder.length) {
+						fusion_privacy_placeholder.siblings('.fluid-width-video-wrapper').hide();
+						var padding,
+							parent = fusion_privacy_placeholder.parent(),
+							prev = fusion_privacy_placeholder.prev(),
+							width = fusion_privacy_placeholder.outerWidth(),
+							height = fusion_privacy_placeholder.outerHeight();
 
-					if (!(parent.hasClass("fusion-background-video-wrapper") || parent.hasClass("fluid-width-video-wrapper"))) {
-						padding = (height / width * 100) + "%";
-						fusion_privacy_placeholder.wrap('<div class="fluid-width-video-wrapper" style="padding-top:' + padding + '" />');
-						fusion_privacy_placeholder.parent().append(prev)
+						if (!(parent.hasClass("fusion-background-video-wrapper") || parent.hasClass("fluid-width-video-wrapper"))) {
+							padding = (height / width * 100) + "%";
+							fusion_privacy_placeholder.wrap('<div class="fluid-width-video-wrapper" style="padding-top:' + padding + '" />');
+							fusion_privacy_placeholder.parent().append(prev)
+						}
 					}
+					resize_linked_videos();
+				});
+			}
+
+			var embedContainer = $(el).parent();
+			lazySizes.rAF(function () {
+				image.addClass('lazyload');
+
+				if (embedContainer.children('img').is('.lazyload, .lazyloading')) {
+					embedContainer.children('img').on('lazyload', resizeContainer);
+				} else {
+					resizeContainer();
 				}
 
-				resize_linked_videos();
-			}
-
-			var embedContainer = $(this).parent();
+				$(el).data('lazyLoadedVideo', true);
+			})
 			var resizeContainer = lazySizes.debounce(lazySizes.rAFIt(function () {
 				var embedContainerClientHeight = embedContainer.children('img').get(0).clientHeight;
 				var embedContainerHeight = parseFloat(embedContainer.css('height').replace('px', ''));
@@ -84,22 +108,15 @@
 					embedContainer.attr('height', embedContainerClientHeight + 'px');
 				}
 			}, true));
-
-			if (embedContainer.children('img').is('.lazyload, .lazyloading')) {
-				embedContainer.children('img').on('lazyload', resizeContainer);
-			} else {
-				resizeContainer();
-			}
-
-			$(this).data('lazyLoadedVideo', true);
 		}
 	});
 
 	// Avada/Fusion privacy box compatibility
-
-	$('.fusion-privacy-placeholder').find('.fusion-privacy-consent').on('click', function () {
-		$('.fusion-privacy-consent').each(function () {
-			$(this).closest('.fluid-width-video-wrapper').siblings('.fluid-width-video-wrapper').show().end().remove();
+	lazySizes.rAF(function () {
+		$('.fusion-privacy-placeholder').find('.fusion-privacy-consent').on('click', function () {
+			$('.fusion-privacy-consent').each(function () {
+				$(this).closest('.fluid-width-video-wrapper').siblings('.fluid-width-video-wrapper').show().end().remove();
+			});
 		});
 	});
 
@@ -126,8 +143,10 @@
 				//Restore VC Composer Video wrapper element padding
 				var vc_video_wrapper = $video.closest('.wpb_video_wrapper');
 				if (vc_video_wrapper.length) {
-					vc_video_wrapper.css('padding-top', vc_video_wrapper.data('videoPadding'));
-					vc_video_wrapper.removeData('videoPadding');
+					lazySizes.rAF(function () {
+						vc_video_wrapper.css('padding-top', vc_video_wrapper.data('videoPadding'));
+						vc_video_wrapper.removeData('videoPadding');
+					});
 				}
 				var embedContainer = $video.siblings('[data-lazy-video-embed-container]').add($video.prevAll('p').find('[data-lazy-video-embed-container]')).add($video.siblings('.fluid-width-video-wrapper').children('[data-lazy-video-embed-container]'));
 				if (embedContainer.length) {
@@ -159,24 +178,26 @@
 				var fusion_video_wrapper = $video.closest('.fusion-video, .wpb_wrapper');
 				if (fusion_video_wrapper.length) {
 					if ($video.is('[data-privacy-src-disabled]')) {
-						$video.removeClass('fusion-hidden');
+						lazySizes.rAF(function () {
+							$video.removeClass('fusion-hidden');
+						});
 					}
 				}
 
 				var divi_video_wrapper = $video.closest('.et_pb_video, .et_main_video_container, .et_pb_video_wrap');
 				if (divi_video_wrapper.length) {
-					divi_video_wrapper.show();
-					if ($.fn.fitVids) {
-						var embedContainerHeight = parseFloat(embedContainer.css('height').replace('px', ''));
-						embedContainer.attr('width', embedContainer.css('width').replace('px', ''));
-						embedContainer.attr('height', embedContainerHeight);
-					}
-					divi_video_wrapper.fitVids({ customSelector: '[data-lazy-video-embed-container]' });
+					lazySizes.rAF(function () {
+						divi_video_wrapper.show();
+						if ($.fn.fitVids) {
+							var embedContainerHeight = parseFloat(embedContainer.css('height').replace('px', ''));
+							embedContainer.attr('width', embedContainer.css('width').replace('px', ''));
+							embedContainer.attr('height', embedContainerHeight);
+						}
+						divi_video_wrapper.fitVids({ customSelector: '[data-lazy-video-embed-container]' });
+					});
 				}
 			}
 		}
-
-		//$this.triggerHandler('load');
 	});
 
 	function resize_linked_videos () {
@@ -195,23 +216,25 @@
 
 	// Avada Fusion/Visual Composer Resize Compatibility
 	$(function () {
-		$('.fusion-video[class*="video-size-linked-to-"]').each(function () {
-			var classes = $(this).attr("class").split(' ');
-			var id = classes.filter(function (item) {
-				return item.startsWith('video-size-linked-to-');
+		lazySizes.rAF(function () {
+			$('.fusion-video[class*="video-size-linked-to-"]').each(function () {
+				var classes = $(this).attr("class").split(' ');
+				var id = classes.filter(function (item) {
+					return item.startsWith('video-size-linked-to-');
+				})
+				id = id.pop().replace('video-size-linked-to-', '');
+				$(this).find('img').attr('data-size-linked-to', id);
+				$(this).removeClass('video-size-linked-to-' + id);
 			})
-			id = id.pop().replace('video-size-linked-to-', '');
-			$(this).find('img').attr('data-size-linked-to', id);
-			$(this).removeClass('video-size-linked-to-' + id);
-		})
-		resize_linked_videos();
+			resize_linked_videos();
+		});
 	});
 
 	/* Divi Builder Video Overlay Workaround */
 	$(function () {
 		if ($('.et_pb_video_overlay').length) {
 			var old_et_pb_play_overlayed_video = null;
-			(function check () {
+			var check = lazySizes.rAFIt(function () {
 				var func = window.et_pb_play_overlayed_video;
 				var is_overridden = func !== old_et_pb_play_overlayed_video;
 				var original_exists = old_et_pb_play_overlayed_video !== null;
@@ -238,7 +261,8 @@
 					}
 				}
 				setTimeout(check, 10);
-			})();
+			});
+			check();
 		}
 	});
 
