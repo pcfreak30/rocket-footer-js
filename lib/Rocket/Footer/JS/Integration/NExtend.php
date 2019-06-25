277fa39ab8420cd70563fdaeaa41c4a792a79c27
Derrick Hammer <derrick@derrickhammer.com>
2019-06-25 01:36:32 -0400

    *add further compatibility to the NExtend framework, specific to smart slider to force the js to render on window load rather than document ready.

--- lib/Rocket/Footer/JS/Integration/NExtend.php
+++ lib/Rocket/Footer/JS/Integration/NExtend.php
@@ -6,11 +6,14 @@ namespace Rocket\Footer\JS\Integration;
 
 class NExtend extends IntegrationAbstract {
 
+	private $override_added = false;
+
 	public function init() {
 		if ( class_exists( 'N2Pluggable' ) ) {
 			\N2Pluggable::addAction( 'systemglobal', [ $this, 'disable' ] );
 			\N2Settings::init();
 		}
+		add_action( 'plugins_loaded', [ $this, 'check' ], 21 );
 	}
 
 	public function disable( $referenceKey, &$rows ) {
@@ -26,5 +29,22 @@ class NExtend extends IntegrationAbstract {
 				$rows[ $key ]['value'] = 0;
 			}
 		}
+	}
+
+	public function check() {
+		if ( class_exists( 'N2SmartsliderApplicationInfo' ) ) {
+			\N2AssetsManager::getInstance();
+			\N2AssetsManager::disableCacheAll();
+			add_filter( 'do_shortcode_tag', [ $this, 'maybe_add_override_js' ], 10, 2 );
+		}
+	}
+
+	public function maybe_add_override_js( $output, $tag ) {
+		if ( in_array( $tag, [ 'smartslider3', 'fusion_smartslider3' ] ) && ! $this->override_added ) {
+			\N2JS::addCode( '(function(){var original=N2Classes.SmartSliderAbstract.prototype.constructor;N2Classes.SmartSliderAbstract.prototype.constructor=function(){var that = this;var args=[].slice.call(arguments);jQuery(window).load(function(){original.apply(that,args)})}})();', 'smartslider-frontend-override' );
+			$this->override_added = true;
+		}
+
+		return $output;
 	}
 }
