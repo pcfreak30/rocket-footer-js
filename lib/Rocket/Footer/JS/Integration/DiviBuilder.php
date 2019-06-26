25c4c84ad7107295e6c9b496c46258bc4ad7391c
Derrick Hammer <derrick@derrickhammer.com>
2019-06-26 18:02:45 -0400

    *Optimize post query

--- lib/Rocket/Footer/JS/Integration/DiviBuilder.php
+++ lib/Rocket/Footer/JS/Integration/DiviBuilder.php
@@ -50,6 +50,8 @@ class DiviBuilder extends IntegrationAbstract {
 				$attachments = get_posts( [
 					'post_type'        => 'attachment',
 					'suppress_filters' => false,
+					'posts_per_page'   => 1,
+					'order_by'         => 'none',
 				] );
 				remove_filter( 'posts_where_paged', [ $this, 'filter_where' ] );
 				$attachment_id = 0;
