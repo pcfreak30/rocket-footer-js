9ac3000af0038805a2aabe8a31c3f2f98e2062c9
Derrick Hammer <derrick@derrickhammer.com>
2019-11-28 04:12:22 -0500

    *Add support for purging cache via cron if the rocket_footer_js_background_cache_purge_item_threshold filter is hit, and only preload cache after purging

--- lib/Rocket/Footer/JS/Cache/Manager.php
+++ lib/Rocket/Footer/JS/Cache/Manager.php
@@ -30,11 +30,15 @@ class Manager extends Component {
 
 	public function init() {
 		add_action( 'after_rocket_clean_domain', [ $this, 'purge_cache' ], 10, 0 );
-		add_action( 'after_rocket_clean_domain', [ $this, 'do_preload' ], 10, 0 );
 		add_action( 'wp_rocket_start_preload', 'run_rocket_sitemap_preload', 10, 0 );
 		add_action( 'after_rocket_clean_post', [ $this, 'purge_post' ] );
 		add_action( 'after_rocket_clean_term', [ $this, 'purge_term' ] );
 		add_action( 'after_rocket_clean_file', [ $this, 'purge_url' ] );
+		add_action( 'wp_rocket_start_preload', 'run_rocket_sitemap_preload', 10, 0 );
+		add_action( 'rocket_footer_js_purge_cache', [ $this, 'do_purge_cache' ] );
+		if ( is_admin() && $this->get_admin_cache_flag() ) {
+			add_action( 'admin_notices', [ $this, 'cache_purge_notice' ] );
+		}
 		$this->store->set_prefix( JS::TRANSIENT_PREFIX );
 		$interval = 0;
 		if ( function_exists( 'get_rocket_purge_cron_interval' ) ) {
@@ -44,6 +48,52 @@ class Manager extends Component {
 		$this->store->set_max_branch_length( apply_filters( 'rocket_footer_js_max_branch_length', 50 ) );
 	}
 
+	private function get_admin_cache_flag() {
+		return get_transient( $this->get_admin_cache_flag_name() );
+	}
+
+	private function get_admin_cache_flag_name() {
+		return "{$this->plugin->get_safe_slug()}_cache_purging";
+	}
+
+	public function purge_cache() {
+		if ( $this->get_admin_cache_flag() && ! wp_next_scheduled( 'rocket_footer_js_purge_cache' ) ) {
+			$this->clear_admin_cache_flag();
+		}
+		if ( $this->get_admin_cache_flag() ) {
+			return;
+		}
+		$size = $this->store->get_cache_fragment( [ 'count' ] );
+
+		if ( $size > apply_filters( 'rocket_footer_js_background_cache_purge_item_threshold', 25, $size ) && ! wp_doing_cron() ) {
+			wp_schedule_single_event( time(), 'rocket_footer_js_purge_cache' );
+			$this->set_admin_cache_flag();
+
+			return;
+		}
+		$this->do_purge_cache();
+	}
+
+	private function set_admin_cache_flag() {
+		set_transient( $this->get_admin_cache_flag_name(), true, DAY_IN_SECONDS );
+	}
+
+	public function do_purge_cache() {
+		$this->set_admin_cache_flag();
+		$this->store->delete_cache_branch();
+		$this->delete_minify_files();
+		$this->clear_admin_cache_flag();
+		$this->do_preload();
+	}
+
+	private function delete_minify_files() {
+		rocket_rrmdir( $this->plugin->get_cache_path() );
+	}
+
+	private function clear_admin_cache_flag() {
+		delete_transient( "{$this->plugin->get_safe_slug()}_cache_purging" );
+	}
+
 	function do_preload() {
 		if ( wp_doing_cron() ) {
 			run_rocket_sitemap_preload();
@@ -51,14 +101,6 @@ class Manager extends Component {
 			return;
 		}
 		wp_schedule_single_event( time(), 'wp_rocket_start_preload' );
-	}
-
-	/**
-	 *
-	 */
-	public function purge_cache() {
-		$this->store->delete_cache_branch();
-		rocket_rrmdir( $this->plugin->get_cache_path() );
 	}
 
 	/**
@@ -105,5 +147,12 @@ class Manager extends Component {
 	 */
 	public function get_store() {
 		return $this->store;
+	}
+
+	public function cache_purge_notice() {
+		$class   = 'notice notice-info';
+		$message = __( sprintf( '%s is currently purging the JS minify cache in the background', $this->plugin->get_plugin_info( 'Name' ) ), $this->plugin->get_safe_slug() );
+
+		printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
 	}
 }
